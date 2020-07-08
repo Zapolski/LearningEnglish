b053991e68727fc65b128f19e76bdb4ff9c0dda0
Serg <nestor.s@mail.ru>
2020-07-08 21:06:31 +0300

    Initial commit

---
+++ learning-english-web/src/main/java/by/zapolski/english/LearningEnglishWebApplication.java
@@ -0,0 +1,13 @@
+package by.zapolski.english;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningEnglishWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningEnglishWebApplication.class, args);
+	}
+
+}
---
+++ learning-english-web/src/test/java/by/zapolski/english/LearningEnglishWebApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zapolski.english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningEnglishWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
